fc1055464539f134e0d7e6c5fe255a9c1db91f7e
Marcos Hernan Parella <marcosparella2000@gmail.com>
2023-03-28 23:59:13 -0300

    Nuevas cosas

--- App.tsx
+++ App.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
+// import {FlexScreen} from './src/screens/FlexScreen';
+import {TareaScreen} from './src/screens/TareaScreen';
 // import {BoxObjectModelScreen} from './src/screens/BoxObjectModelScreen';
 // import {DimensionesScreen} from './src/screens/DimensionesScreen';
-import {PositionScreen} from './src/screens/PositionScreen';
+// import {PositionScreen} from './src/screens/PositionScreen';
 // import {ContadorScreen} from './src/screens/ContadorScreen';
 // import {HolaMundoScreen} from './src/screens/HolaMundoScreen';
 export const App = () => {
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={{flex: 1, backgroundColor: '28425B'}}>
       {/* <HolaMundoScreen />
        <ContadorScreen /> */}
       {/* <BoxObjectModelScreen /> */}
       {/* <DimensionesScreen /> */}
-      <PositionScreen />
+      {/* <PositionScreen /> */}
+      {/* <FlexScreen /> */}
+      <TareaScreen />
     </SafeAreaView>
   );
 };
---
+++ src/screens/FlexScreen.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+export const FlexScreen = () => {
+  return (
+    <View style={style.container}>
+      <Text style={style.caja1}>Caja 1</Text>
+      <Text style={style.caja2}>Caja 2</Text>
+      <Text style={style.caja3}>Caja 3</Text>
+    </View>
+  );
+};
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#28C4d9',
+  },
+  caja1: {
+    borderWidth: 2,
+    borderColor: 'white',
+    fontSize: 30,
+  },
+  caja2: {
+    borderWidth: 2,
+    borderColor: 'white',
+    fontSize: 30,
+  },
+  caja3: {
+    borderWidth: 2,
+    borderColor: 'white',
+    fontSize: 30,
+  },
+});
